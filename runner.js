15ad6283ec71ef4ec5d4eb9afc94bbbbf0aed5d6
Jess Telford <my@email>
2015-03-30 21:09:06 +1100

    Loop over given lessons to add them

--- runner.js
+++ runner.js
@@ -4,9 +4,18 @@
 var packageJson = require('./package.json'),
     adventure = require('adventure');
 
-var shop = adventure(packageJson.name);
+var shop = adventure(packageJson.name),
+    lesson;
 
-shop.add('lesson 1', function () { return require('./lesson-1') });
-/* shop.add('lesson 2', function () { return require('./lesson-2') }); */
+[
+  'traditional-looping'
+].forEach(function(lesson, index) {
+
+  lesson = require('./' + lesson);
+
+  shop.add((index + 1) + '. ' + lesson.title, function() {
+    return lesson.problem
+  });
+})
 
 shop.execute(process.argv.slice(2));
--- traditional-looping/index.js
+++ traditional-looping/index.js
@@ -1,13 +1,14 @@
-var msee = require('msee'),
-    path = require('path'),
-    verify = require('adventure-verify'),
+var path = require('path'),
     problem = require('../problem');
 
-module.exports = problem(__dirname, function (args, t) {
+module.exports = {
+  title: 'Traditional Looping',
+  problem: problem(__dirname, function (args, t) {
     var f = require(path.resolve(args[0]));
     t.equal(typeof f, 'function', 'you exported a function');
     t.equal(f(2,3), 6, '2 * 3 = 6');
     t.equal(f(1,1), 1, '1 * 1 = 1');
     t.equal(f(0.5,0.5), 0.25, '0.5 * 0.5 = 0.25');
     t.end();
-});
+  })
+}
